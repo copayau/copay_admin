365409145c4e6343c144d11f4f5a4633f860c7b5
bdipesh <bdipesh08@gmail.com>
2025-11-12 12:33:12 +0545

    remvoed

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useContactStore, type ContactType } from '@/store/contactStore.ts';
 import Table, { type TableColumn } from '@/components/ui/Table';
 
